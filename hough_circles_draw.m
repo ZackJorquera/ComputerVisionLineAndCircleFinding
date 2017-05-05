db7a5ebf44b93274d533bba9aedeffaf2bf6eb19
Zack Jorquera <jorquerazack@gmail.com>
2017-05-05 15:52:36 -0600

    Update hough_circles_draw.m

--- hough_circles_draw.m
+++ hough_circles_draw.m
@@ -1,8 +1,4 @@
 function hough_circles_draw(peaks)
-%HOUGH_CIRCLES_DRAW Summary of this function goes here
-%   Detailed explanation goes here
-    %imshow(BW);
-    %hold on;
 
     for i = 1:size(peaks)
         th = 0:pi/50:2*pi;
@@ -11,8 +7,6 @@ function hough_circles_draw(peaks)
         yunit = peaks(i,1) - (radius * sin(th)) - radius;
         plot(xunit, yunit,'Color','green', 'LineWidth', 2);
     end
-
-    %hold off;
         
 end
 
