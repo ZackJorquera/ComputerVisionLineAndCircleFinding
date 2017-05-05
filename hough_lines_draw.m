957306dc8f109c4926a64f5298e70a1bd0028125
Zack Jorquera <jorquerazack@gmail.com>
2017-05-05 15:54:25 -0600

    Update hough_lines_draw.m

--- hough_lines_draw.m
+++ hough_lines_draw.m
@@ -2,14 +2,9 @@ function hough_lines_draw(img, peaks, rho, theta)
     % Draw lines found in an image using Hough transform.
     %
     % img: Image on top of which to draw lines
-    % //outfile: Output image filename to save plot as
     % peaks: Qx2 matrix containing row, column indices of the Q peaks found in accumulator
     % rho: Vector of rho values, in pixels
     % theta: Vector of theta values, in degrees
-
-    % TODO: Your code here
-    %imshow(img);
-    %hold on;
     
     for i = 1:size(peaks,1)
         if(theta(peaks(i,2)) == 90)
